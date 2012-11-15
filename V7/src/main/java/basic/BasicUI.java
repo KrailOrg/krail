9276a04c79ca7d5432a6fe0d3559646f3336cc6c
david <david.sowerby@virgin.net>
2012-11-15 20:48:31 +0000

    uriFragment handled

--- V7/src/main/java/basic/BasicUI.java
+++ V7/src/main/java/basic/BasicUI.java
@@ -2,14 +2,15 @@ package basic;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import com.vaadin.server.Page.UriFragmentChangedEvent;
+import com.vaadin.server.Page.UriFragmentChangedListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-public class BasicUI extends UI {
+public class BasicUI extends UI implements UriFragmentChangedListener {
 
 	@Inject
 	@Named("title")
@@ -17,23 +18,33 @@ public class BasicUI extends UI {
 
 	@Inject(optional = true)
 	@Named("version")
-	private final String version = "Vaadin <i>7.0.0 beta9</i>";
+	private final String version = "Vaadin <b>7.0.0 beta9</b>";
 
 	@Inject
 	private MessageSource msgSource;
+	private Label fragmentLabel;
 
 	@Override
 	protected void init(VaadinRequest request) {
 		getPage().setTitle(title);
+		getPage().addUriFragmentChangedListener(this);
 
 		Label label = new Label(version + ", " + msgSource.msg(), ContentMode.HTML);
-		label.setSizeUndefined();
+		label.setHeight("100px");
 
-		VerticalLayout layout = new VerticalLayout(label);
-		layout.setSizeFull();
-		layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+		fragmentLabel = new Label("url fragment = " + getPage().getUriFragment());
+		fragmentLabel.setHeight("100px");
+
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponent(label);
+		layout.addComponent(fragmentLabel);
 
 		setContent(layout);
+	}
+
+	@Override
+	public void uriFragmentChanged(UriFragmentChangedEvent event) {
+		fragmentLabel.setValue("uri fragment changed to:" + event.getUriFragment());
 	}
 
 }
