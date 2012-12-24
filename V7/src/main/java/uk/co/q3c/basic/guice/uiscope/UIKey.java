95156d7412d3324701b5d0292fc7207f5e00b945
david <david.sowerby@virgin.net>
2012-12-24 13:57:22 +0000

    Documentation

--- V7/src/main/java/uk/co/q3c/basic/guice/uiscope/UIKey.java
+++ V7/src/main/java/uk/co/q3c/basic/guice/uiscope/UIKey.java
@@ -1,10 +1,14 @@
 package uk.co.q3c.basic.guice.uiscope;
 
-
 /**
- * This class is entirely passive - it is a surrogate for MainWindow during the IoC process in support of
- * {@link UIScoped}. It is needed because MainWindow is needed as a key in {@link UIScope}, but because
- * it uses constructor injection itself, is needed before it exists! This class acts a simple surrogate key
+ * This class is entirely passive - it is a surrogate for the UI itself during the IoC process in support of
+ * {@link UIScoped}. <br>
+ * <br>
+ * The UI instance would normally be used as the key in @link {@link UIScope}, but this causes a problem with
+ * constructor injection of a UI instance. This is because any constructor parameters which are also UIScoped are
+ * created before the UI, and therefore before the UI entry in UIScope exists. To overcome this, the UI is represented
+ * by a {@link UIKey}, which is available from the start of UI construction. The UI itself, and any UIScoped injections
+ * are then linked by that {@link UIKey} instance.
  * 
  */
 public class UIKey {
