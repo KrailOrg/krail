017591c775253f69e196da9aed007f1f25f7faad
David Sowerby <david.sowerby@virgin.net>
2013-05-14 18:26:15 +0100

    Tidied up comments

--- V7/src/main/java/uk/co/q3c/v7/demo/view/DemoModule.java
+++ V7/src/main/java/uk/co/q3c/v7/demo/view/DemoModule.java
@@ -1,15 +1,12 @@
 package uk.co.q3c.v7.demo.view;
 
-import uk.co.q3c.v7.base.navigate.URIFragmentHandler;
-import uk.co.q3c.v7.base.view.V7View;
 import uk.co.q3c.v7.util.A;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
 /**
- * This becomes the site map. Simply map a virtual page (see @link {@link URIFragmentHandler}) to the {@link V7View}
- * class which will represent it
+ * Some application specific String definitions
  * 
  * @author david
  * 
@@ -22,27 +19,10 @@ public class DemoModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		// some Strings for the demo
 		bind(String.class).annotatedWith(Names.named(A.title)).toInstance(
-				"Guice Vaadin and Shiro demonstration application");
+				"Guice, Vaadin and Shiro demonstration application");
 		bind(String.class).annotatedWith(Names.named(A.version)).toInstance("Vaadin 7.0.5");
 
 	}
-
-	// @Override
-	// protected void bindViews(MapBinder<String, V7View> mapbinder) {
-	// mapbinder.addBinding("").to(PublicHomeView.class);
-	// mapbinder.addBinding("public/home").to(PublicHomeView.class);
-	// mapbinder.addBinding("secure/home").to(SecureHomeView.class);
-	// mapbinder.addBinding("secure/view1").to(View1.class);
-	// mapbinder.addBinding("public/view2").to(View2.class);
-	// mapbinder.addBinding("public/login").to(LoginView.class);
-	// mapbinder.addBinding("public/logout").to(LogoutView.class);
-	// mapbinder.addBinding("public/reset-account").to(AccountRequestView.class);
-	// mapbinder.addBinding("public/unlock-account").to(AccountRequestView.class);
-	// mapbinder.addBinding("public/refresh-account").to(AccountRequestView.class);
-	// mapbinder.addBinding("public/request-account").to(AccountRequestView.class);
-	// mapbinder.addBinding("public/enable-account").to(AccountRequestView.class);
-	// }
 
 }
