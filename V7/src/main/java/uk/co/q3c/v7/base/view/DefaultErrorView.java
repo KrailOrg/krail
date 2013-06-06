7b6241fa07b2f3e0f1e603d79fcb6556e6547fc1
lelmarir <lelmarir@gmail.com>
2013-06-06 10:10:11 +0200

    removed reference to uk.co.q3c.v7.demo.view pakage

--- V7/src/main/java/uk/co/q3c/v7/base/view/DefaultErrorView.java
+++ V7/src/main/java/uk/co/q3c/v7/base/view/DefaultErrorView.java
@@ -4,29 +4,31 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import uk.co.q3c.v7.demo.view.DemoViewBase;
-import uk.co.q3c.v7.demo.view.components.FooterBar;
-import uk.co.q3c.v7.demo.view.components.HeaderBar;
+import com.vaadin.ui.Label;
 
-import com.vaadin.ui.Button;
-import com.vaadin.ui.themes.ChameleonTheme;
+public class DefaultErrorView extends VerticalViewBase implements ErrorView {
 
-public class DefaultErrorView extends DemoViewBase implements ErrorView {
-
+	private Label viewLabel;
+	
 	@Inject
-	protected DefaultErrorView(FooterBar footerBar, HeaderBar headerBar) {
-		super(footerBar, headerBar);
-		Button button = addNavButton("take me home", home);
-		button.addStyleName(ChameleonTheme.BUTTON_TALL);
-		getViewLabel().addStyleName("warning");
+	protected DefaultErrorView() {
+
+		//TODO: document how to replace this implementation with teh user one
+		doLayout();
 
 	}
 
+	protected void doLayout() {
+		viewLabel = new Label();
+		viewLabel.addStyleName("warning");
+		addComponent(viewLabel);
+	}
+	
 	@Override
 	public void processParams(List<String> params) {
 		String s = "This is the ErrorView and would say something like \""
 				+ this.getScopedUI().getV7Navigator().getNavigationState() + " is not a valid uri\"";
-		getViewLabel().setValue(s);
+		viewLabel.setValue(s);
 	}
 
 }
