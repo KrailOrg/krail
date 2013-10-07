487f8b36b05f4872b83bb97a4ec11fa9a28b3e3f
lelmarir <lelmarir@gmail.com>
2013-10-07 11:10:28 +0200

    CssLayout and HorizzonalLayout based Views

---
+++ V7/src/main/java/uk/co/q3c/v7/base/view/HorizzontalViewBase.java
@@ -0,0 +1,63 @@
+package uk.co.q3c.v7.base.view;
+
+import java.util.LinkedHashMap;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.co.q3c.v7.base.navigate.V7Navigator;
+import uk.co.q3c.v7.base.ui.ScopedUI;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+
+public abstract class HorizzontalViewBase extends HorizontalLayout implements
+		V7View {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(VerticalViewBase.class);
+	private final V7Navigator navigator;
+
+	@Inject
+	protected HorizzontalViewBase(V7Navigator navigator) {
+		super();
+		this.navigator = navigator;
+	}
+
+	@Override
+	public boolean beforeEnter(V7ViewChangeEvent event) {
+		return true;
+	}
+
+	@Override
+	public void afterEnter(V7ViewChangeEvent event) {
+		LOGGER.debug("entered view: " + this.getClass().getSimpleName()
+				+ " with uri: "
+				+ event.getNewNavigationState().getFragment().getUri());
+		processParams(event.getNewNavigationState().getFragment()
+				.getParameters());
+	}
+
+	/**
+	 * typecasts and returns getUI()
+	 * 
+	 * @return
+	 */
+
+	@Override
+	public ScopedUI getUI() {
+		return (ScopedUI) super.getUI();
+	}
+
+	protected abstract void processParams(LinkedHashMap<String, String> params);
+
+	@Override
+	public Component getRootComponent() {
+		return this;
+	}
+
+	public V7Navigator getNavigator() {
+		return navigator;
+	}
+}
