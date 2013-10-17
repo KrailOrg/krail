5c68ee0dd2a65845653e416f5cb930e746a54600
lelmarir <lelmarir@gmail.com>
2013-10-17 17:51:42 +0200

    injected a provider instead of the sitemap directly

--- V7/src/main/java/uk/co/q3c/v7/base/view/ApplicationViewService.java
+++ V7/src/main/java/uk/co/q3c/v7/base/view/ApplicationViewService.java
@@ -1,6 +1,7 @@
 package uk.co.q3c.v7.base.view;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 import uk.co.q3c.v7.base.guice.services.Service;
@@ -12,15 +13,18 @@ import uk.co.q3c.v7.base.navigate.sitemap.Sitemap;
 @Service
 public class ApplicationViewService {
 
-	// the sitemap will be created right after the injector, this way many
-	// errors could be seen earlier than first use
+	private final Provider<Sitemap> sitemapProvider;
+	
 	@Inject
-	public ApplicationViewService(Sitemap sitemap) {
+	public ApplicationViewService(Provider<Sitemap> sitemapProvider) {
+		this.sitemapProvider = sitemapProvider;
 	}
 
 	@Start
 	public void start() throws Exception {
-		;
+		// the sitemap will be created right after the injector, this way many
+		// errors could be seen earlier than first use
+		sitemapProvider.get();
 	}
 
 	@Stop
