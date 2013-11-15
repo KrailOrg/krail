375183d20dd22f15c40988c8fd78086bd1cd7622
David Sowerby <david.sowerby@virgin.net>
2013-11-15 15:45:55 +0000

    Added a dummy test so Git saves the test folder in the orient project

---
+++ orient/src/test/java/uk/co/q3c/v7/persist/orient/custom/OrientCustomType_MoneyTest.java
@@ -0,0 +1,22 @@
+package uk.co.q3c.v7.persist.orient.custom;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * This does nothing yet, it is just here to ensure git stores the test directory
+ * 
+ * @author david
+ * 
+ */
+public class OrientCustomType_MoneyTest {
+
+	@Ignore
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
