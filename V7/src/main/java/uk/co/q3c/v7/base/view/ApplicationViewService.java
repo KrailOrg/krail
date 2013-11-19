c0f02f4ee956a793e807e2527db01ba900566151
David Sowerby <david.sowerby@virgin.net>
2013-11-19 10:12:33 +0000

    Added ApplicationViewService

--- V7/src/main/java/uk/co/q3c/v7/base/view/ApplicationViewService.java
+++ V7/src/main/java/uk/co/q3c/v7/base/view/ApplicationViewService.java
@@ -1,5 +1,7 @@
 package uk.co.q3c.v7.base.view;
 
+import javax.inject.Provider;
+
 import uk.co.q3c.v7.base.guice.services.Service;
 import uk.co.q3c.v7.base.guice.services.Start;
 import uk.co.q3c.v7.base.guice.services.Stop;
@@ -13,15 +15,20 @@ import com.google.inject.Singleton;
 // (startAsSoonAsPossible = true)
 public class ApplicationViewService {
 
+	private final Provider<Sitemap> sitemapPro;
+
 	// the sitemap will be created right after the injector, this way many
 	// errors could be seen earlier than first use
 	@Inject
-	public ApplicationViewService(Sitemap sitemap) {
+	public ApplicationViewService(Provider<Sitemap> sitemapPro) {
+		this.sitemapPro = sitemapPro;
 	}
 
 	@Start
 	public void start() throws Exception {
-		;
+		// the sitemap will be created right after the injector, this way many
+		// errors could be seen earlier than first use
+		sitemapPro.get();
 	}
 
 	@Stop
