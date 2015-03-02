c3348a8a832625d585da60b0fd26fafc142d0f1b
David Sowerby <david.sowerby@virgin.net>
2015-03-02 16:02:17 +0000

    Fix #304 Change to logback
    Logback incorporated into the build, LogMonitor updated and configuration files changed.

---
+++ src/test/resources/logback.groovy
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015. David Sowerby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+//
+// Built on Mon Mar 02 14:15:44 CET 2015 by logback-translator
+// For more information on configuration files in Groovy
+// please see http://logback.qos.ch/manual/groovy.html
+
+// For assistance related to this tool or configuration files
+// in general, please contact the logback user mailing list at
+//    http://qos.ch/mailman/listinfo/logback-user
+
+// For professional support please see
+//   http://www.qos.ch/shop/products/professionalSupport
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS}  %-5level %logger{0} - %msg%n"
+    }
+}
+
+
+logger("uk.q3c", DEBUG)
+root(INFO, ["STDOUT", "FILE"])
