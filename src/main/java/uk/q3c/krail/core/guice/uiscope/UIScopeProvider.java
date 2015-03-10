27deccb3545d204cd6b0426f190ec42a1862b7a5
David Sowerby <david.sowerby@virgin.net>
2015-03-10 10:53:26 +0000

    Change to error message only, no functional change

--- src/main/java/uk/q3c/krail/core/guice/uiscope/UIScopeProvider.java
+++ src/main/java/uk/q3c/krail/core/guice/uiscope/UIScopeProvider.java
@@ -1,11 +1,9 @@
 /*
- * Copyright (C) 2014 David Sowerby
- * 
+ * Copyright (c) 2015. David Sowerby
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -50,7 +48,7 @@ class UIScopeProvider<T> implements Provider<T> {
         // this may be null if we are in the process of constructing the UI
         ScopedUI currentUI = (ScopedUI) UI.getCurrent();
         final String msg = "This can happen if you include UIScoped components in your ScopedUIProvider, " +
-                "or you are testing and have not set up the test fixture correctly.  For the latter, " +
+                "or you are testing and have not set up the test fixture correctly.  For the latter, use TestUIScopeModule, or " +
                 "try sub-classing UITestBase and calling createTestUI() or createBasicUI() to prepare the UIScope " +
                 "correctly.  If you are not testing please report a bug";
         if (uiKey == null) {
