8141898b8da9f05c2130167f13037c7ac132f2b1
David Sowerby <david.sowerby@virgin.net>
2015-05-25 11:48:50 +0100

    Grid3x3ViewBase added
    A convenience KrailView class with a 3x3 grid

---
+++ src/main/java/uk/q3c/krail/core/view/Grid3x3ViewBase.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015. David Sowerby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package uk.q3c.krail.core.view;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
+import uk.q3c.krail.core.view.component.ViewChangeBusMessage;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class Grid3x3ViewBase extends ViewBase {
+    private final float[] defaultColumnWidths = new float[]{1f, 1f, 1f};
+    private final float[] defaultRowHeights = new float[]{1f, 1f, 1f};
+    private GridLayout grid;
+
+    @Override
+    protected void doBuild(ViewChangeBusMessage busMessage) {
+
+        grid = new GridLayout(3, 3);
+        grid.setSizeFull();
+        setColumnWidths(defaultColumnWidths);
+        setRowHeights(defaultRowHeights);
+
+
+        setRootComponent(grid);
+
+    }
+
+    /**
+     * Sets the relative widths of the grid columns
+     *
+     * @param relativeWidths
+     *         3 column widths to use
+     *
+     * @throws IllegalArgumentException
+     *         if {@code relativeWidths}  does not have exactly 3 columns
+     */
+    protected void setColumnWidths(float... relativeWidths) {
+        checkArgument(relativeWidths.length == 3);
+        grid.setColumnExpandRatio(0, relativeWidths[0]);
+        grid.setColumnExpandRatio(1, relativeWidths[1]);
+        grid.setColumnExpandRatio(2, relativeWidths[2]);
+    }
+
+    /**
+     * Sets the relative width of the grid rows
+     *
+     * @param relativeHeights
+     *         3 row heightsuse
+     *
+     * @throws IllegalArgumentException
+     *         if {@code relativeHeights}  does not have exactly 3 rows
+     */
+    protected void setRowHeights(float... relativeHeights) {
+        checkArgument(relativeHeights.length == 3);
+        grid.setRowExpandRatio(0, relativeHeights[0]);
+        grid.setRowExpandRatio(1, relativeHeights[1]);
+        grid.setRowExpandRatio(2, relativeHeights[2]);
+    }
+
+    public void setTopLeft(Component component) {
+        grid.addComponent(component, 0, 0);
+    }
+
+    public void setTopCentre(Component component) {
+        grid.addComponent(component, 1, 0);
+    }
+
+    public void setTopRight(Component component) {
+        grid.addComponent(component, 2, 0);
+    }
+
+    public void setMiddleLeft(Component component) {
+        grid.addComponent(component, 0, 1);
+    }
+
+    public void setMiddleCentre(Component component) {
+        grid.addComponent(component, 1, 1);
+    }
+
+    public void setMiddleRight(Component component) {
+        grid.addComponent(component, 2, 1);
+    }
+
+    public void setCentreCell(Component component) {
+        grid.addComponent(component, 1, 1);
+    }
+
+    public void setBottomLeft(Component component) {
+        grid.addComponent(component, 0, 2);
+    }
+
+    public void setBottomCentre(Component component) {
+        grid.addComponent(component, 1, 2);
+    }
+
+    public void setBottomRight(Component component) {
+        grid.addComponent(component, 2, 2);
+    }
+
+    protected GridLayout getGridLayout() {
+        return grid;
+    }
+
+}
---
+++ src/test/java/uk/q3c/krail/core/view/Grid3x3ViewBaseTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2015. David Sowerby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package uk.q3c.krail.core.view;
+
+import com.mycila.testing.junit.MycilaJunitRunner;
+import com.mycila.testing.plugin.guice.GuiceContext;
+import com.vaadin.ui.Label;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.q3c.krail.core.view.component.ViewChangeBusMessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MycilaJunitRunner.class)
+@GuiceContext({})
+public class Grid3x3ViewBaseTest {
+
+    Grid3x3ViewBase view;
+
+    Label topLeft;
+    Label topCentre;
+    Label topRight;
+    Label middleLeft;
+    Label centreCell;
+    Label middleRight;
+    Label bottomLeft;
+    Label bottomCentre;
+    Label bottomRight;
+
+
+    @Mock
+    private ViewChangeBusMessage busMessage;
+
+    @Before
+    public void setup() {
+        view = new Grid3x3ViewBase();
+        topLeft = new Label();
+        topCentre = new Label();
+        topRight = new Label();
+        middleLeft = new Label();
+        centreCell = new Label();
+        middleRight = new Label();
+        bottomLeft = new Label();
+        bottomCentre = new Label();
+        bottomRight = new Label();
+    }
+
+    @Test
+    public void setCells() {
+        //given
+        view.doBuild(busMessage);
+        //when
+        view.setTopLeft(topLeft);
+        view.setTopCentre(topCentre);
+        view.setTopRight(topRight);
+
+        view.setMiddleLeft(middleLeft);
+        view.setCentreCell(centreCell);
+        view.setMiddleRight(middleRight);
+
+        view.setBottomLeft(bottomLeft);
+        view.setBottomCentre(bottomCentre);
+        view.setBottomRight(bottomRight);
+        //then
+        assertThat(view.getGridLayout()
+                       .getComponent(0, 0)).isEqualTo(topLeft);
+        assertThat(view.getGridLayout()
+                       .getComponent(1, 0)).isEqualTo(topCentre);
+        assertThat(view.getGridLayout()
+                       .getComponent(2, 0)).isEqualTo(topRight);
+        assertThat(view.getGridLayout()
+                       .getComponent(0, 1)).isEqualTo(middleLeft);
+        assertThat(view.getGridLayout()
+                       .getComponent(1, 1)).isEqualTo(centreCell);
+        assertThat(view.getGridLayout()
+                       .getComponent(2, 1)).isEqualTo(middleRight);
+        assertThat(view.getGridLayout()
+                       .getComponent(0, 2)).isEqualTo(bottomLeft);
+        assertThat(view.getGridLayout()
+                       .getComponent(1, 2)).isEqualTo(bottomCentre);
+        assertThat(view.getGridLayout()
+                       .getComponent(2, 2)).isEqualTo(bottomRight);
+    }
+
+
+    @Test
+    public void defaultSizes() {
+        //given
+        view.doBuild(busMessage);
+        //when
+
+        //then
+        assertThat(view.getGridLayout()
+                       .getColumnExpandRatio(0)).isEqualTo(view.getGridLayout()
+                                                               .getColumnExpandRatio(1))
+                                                .isEqualTo(view.getGridLayout()
+                                                               .getColumnExpandRatio(2));
+        assertThat(view.getGridLayout()
+                       .getRowExpandRatio(0)).isEqualTo(view.getGridLayout()
+                                                            .getRowExpandRatio(1))
+                                             .isEqualTo(view.getGridLayout()
+                                                            .getRowExpandRatio(2));
+    }
+}
