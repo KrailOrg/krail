39b7a34a8e3a90909ce36ca6598bbfeda5aa0919
David Sowerby <david.sowerby@virgin.net>
2016-01-03 10:44:38 +0000

    Fix #89 LoginView Submit button is default
    'Enter' submits credentials

--- src/main/java/uk/q3c/krail/core/view/DefaultLoginView.java
+++ src/main/java/uk/q3c/krail/core/view/DefaultLoginView.java
@@ -1,22 +1,26 @@
 /*
- * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  * Copyright (c) 2016. David Sowerby
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations under the License.
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.core.view;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.themes.ChameleonTheme;
+import com.vaadin.ui.themes.ValoTheme;
 import net.engio.mbassy.bus.common.PubSubSupport;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authc.*;
@@ -105,6 +109,8 @@ public class DefaultLoginView extends Grid3x3ViewBase implements LoginView, Clic
     protected void setIds() {
         super.setIds();
         submitButton.setId(ID.getId(Optional.empty(), this, submitButton));
+        submitButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+        submitButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
         usernameBox.setId(ID.getId(Optional.of("username"), this, usernameBox));
         passwordBox.setId(ID.getId(Optional.of("password"), this, passwordBox));
         statusMsgLabel.setId(ID.getId(Optional.of("status"), this, statusMsgLabel));
@@ -170,13 +176,13 @@ public class DefaultLoginView extends Grid3x3ViewBase implements LoginView, Clic
     }
 
     @Override
-    public void setStatusMessage(String msg) {
-        statusMsgLabel.setValue(msg);
+    public void setStatusMessage(I18NKey messageKey) {
+        setStatusMessage(translate.from(messageKey));
     }
 
     @Override
-    public void setStatusMessage(I18NKey messageKey) {
-        setStatusMessage(translate.from(messageKey));
+    public void setStatusMessage(String msg) {
+        statusMsgLabel.setValue(msg);
     }
 
     public TextField getUsernameBox() {
