58fd839d514a139cefbf0501fe0bc5bdca99068d
David Sowerby <david.sowerby@virgin.net>
2017-08-17 14:35:04 +0100

    Moved JPAContainer interfaces in to the core from krail-jpa to remove krail-jpa dependency on Vaadin

---
+++ src/main/java/uk/q3c/krail/core/option/jpa/JpaContainerProvider.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * Copyright (c) 2016. David Sowerby
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package uk.q3c.krail.core.option.jpa;
+
+import com.vaadin.addon.jpacontainer.JPAContainer;
+import com.vaadin.addon.jpacontainer.JPAContainerFactory;
+import uk.q3c.krail.persist.ContainerType;
+import uk.q3c.krail.persist.VaadinContainerProvider;
+
+/**
+ * A Krail replacement for {@link JPAContainerFactory}
+ * <p>
+ * Created by David Sowerby on 03/01/15.
+ */
+public interface JpaContainerProvider extends VaadinContainerProvider {
+
+    <E> JPAContainer<E> get(Class<E> entityClass, ContainerType containerType);
+}
