cc1edba312d6d759db978de2a60b104fa40b3305
David Sowerby <david.sowerby@virgin.net>
2018-08-28 10:11:30 +0100

    Additional CommonLabelKey s

--- src/main/kotlin/uk/q3c/krail/core/i18n/CommonLabelKey.kt
+++ src/main/kotlin/uk/q3c/krail/core/i18n/CommonLabelKey.kt
@@ -6,5 +6,15 @@ import uk.q3c.krail.i18n.I18NKey
  * Created by David Sowerby on 26 Aug 2018
  */
 enum class CommonLabelKey : I18NKey {
-    After, Before, No, Yes
+    After,
+    Before,
+    Full_Screen,
+    Home,
+    Menu,
+    Log_Out,
+    Notifications,
+    No,
+    Settings,
+    Title,
+    Yes
 }
